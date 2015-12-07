eacc736541dbd0acaeb5731c3f10bc82c65736c7
kadironay <kadironay@gmail.com>
2015-12-07 21:42:41 +0200

    Webview added.

--- MyApplication/app/src/main/java/com/tahminbudur/tahminbudur/MainActivity.java
+++ MyApplication/app/src/main/java/com/tahminbudur/tahminbudur/MainActivity.java
@@ -1,13 +1,27 @@
 package com.tahminbudur.tahminbudur;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final WebView mainWebView = (WebView) findViewById(R.id.mainWebView);
+
+        String link = "http://m.tahminbudur.com";
+        mainWebView.loadUrl(link);
+        mainWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
     }
 }
